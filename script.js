acbd8d025cc15bb97df7510283a1203ac5635d85
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-02 10:53:00 +0300

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content has been added by JavaScript";
+
+document.querySelector("body").appendChild(h2);
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "I hear git ReactJS is the new kid on the block!";
+
+document.querySelector("body").appendChild(paragraph);
