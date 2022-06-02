78e58bd839a7c78994f03612a448c2e169b25c8a
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-02 10:54:47 +0300

    minor edits

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ h2.textContent = "This content has been added by JavaScript";
 document.querySelector("body").appendChild(h2);
 
 const paragraph = document.createElement("p");
-paragraph.textContent = "I hear git ReactJS is the new kid on the block!";
+paragraph.textContent = "I hear ReactJS is the new kid on the block!";
 
 document.querySelector("body").appendChild(paragraph);
